6fe63530a2b2c86ae83330fae7dabc9b6378b3d2
RagavendranRj05 <rj05ragavendran@gmail.com>
2025-12-18 12:54:37 +0530

    first commit

---
+++ loop.py
@@ -0,0 +1,10 @@
+# while(10<5):
+#     print("Python")
+
+li = [1,2,3,4,5]
+
+li1 = [1,2,3,4,5]
+
+print(id(li))
+
+print(id(li1))
---
+++ sample.py
@@ -0,0 +1,98 @@
+# What is Python?
+
+# Python is a popular programming language. It was created by Guido van Rossum,
+#  and released in 1991.
+
+
+# It is used for:
+
+# 1. web development (server-side),
+# 2. software development,
+# 3. mathematics,
+# 4. system scripting.
+
+
+# Why Python?
+
+# Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+# Python has a simple syntax similar to the English language.
+# Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+# Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+# Python can be treated in a procedural way, an object-oriented way or a functional way.
+
+
+# KEY FEATURES IN PYTHON
+
+
+# Feature	            Description
+# Simple Syntax	    Easy to read and write
+# Interpreted   	    Runs line by line, no need to compile
+# Open Source	        Free to use
+# Dynamically        Typed No need to declare data types
+# Huge Libraries	    NumPy, Pandas, Django, Flask, TensorFlow
+# Cross-platform	    Runs on Windows, Linux, macOS
+
+
+# syntax
+
+# Syntax refers to the rules that define the structure of code in a programming language.
+
+
+##### Advantages of python
+
+# 1. Simple and Easy to Learn
+
+# Python uses plain English-like keywords, making it beginner-friendly and easy to read.
+
+# 2. Readability and Clean Code
+
+#  Code is more readable with proper indentation instead of using curly braces {}, resulting in clean and clear code.
+
+# 3. Reduced Code Length
+
+#  Python allows you to write fewer lines of code to accomplish tasks compared to other programming languages.
+
+
+# 4.Cross-Platform Compatibility
+
+# Python is platform-independent, meaning it runs on different operating systems (Windows, Linux, macOS) without modification.
+
+# 5.Large Standard Library
+
+# Python comes with a wide range of pre-built libraries (like NumPy, Pandas, Django) for various tasks like web development, data analysis, and machine learning.
+
+# 6.Dynamically Typed Language
+
+# Python automatically determines the type of variable based on the assigned value, so there's no need to declare it explicitly.
+
+# 7.Extensive Support for Integration
+
+# Python easily integrates with other languages (C, Java, .NET) and tools, allowing multi-language project development.
+
+# 8. Open Source and Community Support
+
+# Python is free to use and open-source, with a large developer community that contributes to its growth.
+
+
+# COMMENTS
+
+# Comments can be used to explain Python code.
+
+# Comments can be used to make the code more readable.
+
+# Comments can be used to prevent execution when testing code.
+
+##### .Single-line Comment
+
+# 1. Starts with a # symbol
+
+ 
+# 2. Anything after # is ignored by Python 
+
+
+#####  .Multi-line Comment
+
+
+# 1.Use triple quotes ''' ... ''' or """ ... """  docstring
+
+
