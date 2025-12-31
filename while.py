a5206ac246b51b23367ca387265ea166c62d713c
RagavendranRj05 <rj05ragavendran@gmail.com>
2025-12-31 09:15:45 +0530

    message

---
+++ while.py
@@ -0,0 +1,19 @@
+# i = 0
+
+# while i<5:  # 0<5
+#     print(i)
+
+
+# i = 10
+#      # 5<5            
+# while i<=10:  # 0<5
+#     print(i)
+#     i -= 1 # 0+=1   # - decr
+
+    # 16
+def add():
+    return(9+7)  # -> 16
+
+# a = add()    # 16 -> a
+
+print(add())    # 16
